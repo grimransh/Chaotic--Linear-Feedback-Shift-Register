7e73e85fcafd9f1f4ff81967d02e0afaed636254
ShivamSikri03 <shivamsikri36@gmail.com>
2025-06-21 23:42:04 +0530

    Create quadratic_mapping.v

---
+++ codes/chaotic/quadratic_mapping.v
@@ -0,0 +1,26 @@
+module quadratic_Map ( input clk,reset,              
+											input signed [15:0] x_in , r, 					// It is (-)1.15
+                      output reg signed [15:0] x_out);
+    reg signed [15:0] xn;
+	reg signed [31:0] xn_sq, r_ext, temp;
+
+	always @(posedge clk && reset) begin
+		
+		if(reset) begin
+			x_out<=xn;
+		end
+
+		else begin
+			xn_sq<= xn * xn;
+			r_ext <= r <<<15;   // 1.15 into 2.30
+			temp <= r_ext * xn_sq;
+			xn <= (16'h4000 - temp) >>> 15;
+		end
+
+		x_out <= xn;
+	end
+endmodule
+			
+			
+  
+  
