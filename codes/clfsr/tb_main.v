2a63a0817d47d093e558f040c123ba3ba43b51ca
grimransh <anuragpal572004@gmail.com>
2025-06-23 23:52:49 +0530

    UPDATED main.v  to generate 3 sets of 1byte random number

--- codes/clfsr/tb_main.v
+++ codes/clfsr/tb_main.v
@@ -4,21 +4,37 @@
 module tb_main;
     reg clk;
     reg rst;
-    wire out;
+    wire [7:0] Rout;
+    wire [7:0] Gout;
+    wire [7:0] Bout;
+    wire Key_ready;
     
-    main uut( .clk(clk), .rst(rst), .out(out));
+    integer file;      // Declare file handle
+    integer count = 0; // Declare and initialize count
+    
+    main uut( .clk(clk), .rst(rst), .Rout(Rout), .Gout(Gout), .Bout(Bout), .Key_ready(Key_ready));
 
     initial clk = 0;
     always #5 clk = ~clk;
     initial begin
         $dumpfile("main.vcd");
         $dumpvars(0,tb_main);
-        $monitor("Time=%0t | out=%b", $time, out);
+        // $monitor("Time=%0t | out=%b", $time, out);
         rst = 1;
         #10;
         rst = 0;
-        #1000;
+        file = $fopen("output_bits.txt", "w");
+        #4000000;
+        $fclose(file);
         $finish;
+    end
+
+    always @(posedge clk) begin
+        if (Key_ready) begin
+            // Write Rout, Gout, Bout to file in binary format
+            $fwrite(file, "%b%b%b\n", Rout, Gout, Bout);
+            count = count + 1;
+        end
     end
 
 endmodule
